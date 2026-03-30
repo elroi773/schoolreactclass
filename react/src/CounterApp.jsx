6f084964f3c48526f9da69a0205c39b59bdcd785
김이레 <165885725+elroi773@users.noreply.github.com>
2026-03-30 14:49:48 +0900

    counterapp

--- react/src/CounterApp.jsx
+++ react/src/CounterApp.jsx
@@ -5,16 +5,25 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function CounterApp() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)//상태 기본값 : 0 count 이전 도는 현재 값, setCount :  count 값  변경 함수 
 
   return (
     <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+       <button
+          className="counter"
+          onClick={() => setCount((count) => count + 2)}
+        >
+          +2
+        </button>
       <button
           className="counter"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => setCount((count) => count - 1)}
         >
-          Count is {count}
+          -1
         </button>
+       
     </>
   )
 }

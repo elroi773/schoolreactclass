42e5aa1fd9d64cff28f6b4b24258eac03d145432
김이레 <165885725+elroi773@users.noreply.github.com>
2026-04-13 22:10:10 +0900

    componet 화

---
+++ react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="todo-app__header">
+      <h1 className="todo-app__title">Todo List</h1>
+      <p className="todo-app__subtitle">작은 단위로 나눠서 관리하는 할 일 목록</p>
+    </header>
+  )
+}
